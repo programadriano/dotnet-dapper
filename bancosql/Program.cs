d02668ef363f8c056c1ef656abd0bd61da3c983e
Thiago S Adriano <tadriano.net@gmail.com>
2023-06-17 08:47:47 -0300

    firt commit

---
+++ bancosql/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.SqlClient;
+using Dapper;
+
+// See https://aka.ms/new-console-template for more information
+var host = "SEU_HOST.database.windows.net";
+var user = "SEU_USER";
+var password = "SEU_PASSWORD";
+var database = "SEU_BANCO";
+
+string connectionString = $"Data Source={host};Initial Catalog={database};User ID={user};Password={password};";
+
+
+using (SqlConnection connection = new SqlConnection(connectionString))
+{
+    connection.Open();
+
+    string sql = "SELECT * from Usuarios;";
+    var usuarios = connection.Query(sql).ToList();
+
+    foreach (var row in usuarios)
+    {
+        // Access the returned data using row.ColumnName
+    }
+}
+
+
+Console.ReadLine();
+
